136eb18ed23904b156e7c095b9c8e36e9cb311a9
damxaq <damiancelico@gmail.com>
2021-07-06 16:49:48 +0200

    fixing routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import SinglePost from "./components/SinglePost";
+import Project from "./components/Project";
+import Post from "./components/Post";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Switch>
-        <Route component={Home} path="/" />
+        <Route component={Home} path="/" exact />
         <Route component={About} path="/about" />
         <Route component={SinglePost} path="/post/:slug" />
         <Route component={Post} path="/post" />
